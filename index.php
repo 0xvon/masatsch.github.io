67b477d1a38f3604495448e5405e692d7a37f786
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-02-17 11:24:17 +0900

    practice

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to my page</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
-    <p>hello!!!!!!!!!</p>
+    <div class="box">
+    </div>
 </body>
 
 </html>
